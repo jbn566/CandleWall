b0727d660f8730793dd55bff1aae2981b3c5c5b8
Joshua Allen Bennett <jbn566@gmail.com>
2016-05-22 23:31:19 -0400

    WaxDiscs

--- CodeLibrary/CandleWall.Core/ICandle.cs
+++ CodeLibrary/CandleWall.Core/ICandle.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace JB2.CandleWall
 {
-    public interface ICandle : ICandleWallObject
+    public interface ICandle : ICandleWallObject, JB2.Identity.IPlayerable<string>
     {
         CandleState State { get; set; }
 
         JB2.Common.IPerson<string> Owner { get; set; }
+
+        JB2.Common.JB2Color Color { get; set; }
+
+        IEnumerable<IWaxDisc> WaxDiscs { get; set; }
+
+        DateTime CreationDate { get; set; }
+
+        int GetBurnedWax();
+
+
+
     }
 }
---
+++ CodeLibrary/CandleWall.Core/IWaxDisc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.CandleWall
+{
+    public interface IWaxDisc : ICandleWallObject, Identity.IPlayerable<string>
+    {
+        JB2.Common.JB2Color Color { get; set; }
+        DateTime CreationDate { get; set; }
+        string OrginalCandle { get; set; }
+    }
+}
